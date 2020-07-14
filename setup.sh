f3d88cc051bc062011a2dddbda1256c79de215ad
SokhibjonZamonov <zamonovsokhibjonsysprog@gmail.com>
2020-07-14 22:26:15 +0500

    new commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+export AUTH0_DOMAIN = 'ndfs2020.auth0.com'
+export API_AUDIENCE = 'movie_directors'
+export ALGORITHMS = ['RS256']
+export DATABASE_URL = 'postgres://uvkikegfqtrvpm:5f24008f3936ae8d6c6599b7a57912a41c73df35589ff42d819f806ec498ea91@ec2-50-17-90-177.compute-1.amazonaws.com:5432/dal8d2o27vccbq'
+export FLASK_APP = app.py
